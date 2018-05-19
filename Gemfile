9bf8eeedd09c341c8c6a370d5bf80ef87806c298
takuya <takuyamishima@gmail.com>
2018-05-19 04:49:36 +0000

    管理画面追加

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bcrypt', '3.1.11'
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'activeadmin'
+gem 'devise'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to user_path(user.id)
+      redirect_to books_path
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render 'new'
