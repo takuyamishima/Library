6e3f1e3e75def87bda1ef4d7566bf48e57e9e39f
takuya <takuyamishima@gmail.com>
2018-05-21 14:00:02 +0000

    予約した本の名前が出るように

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,17 @@ class FavoritesController < ApplicationController
 
   def create
     favorite = current_user.favorites.create(book_id: params[:book_id])
+    
     if favorite.save
         BookingMailer.booking_mail(@booking).deliver  ##追記
-    
-    redirect_to books_url, notice: "#本を予約しました"
+    @favorite_book_name = favorite.book.book_name
+    redirect_to books_url, notice: "#{favorite.book.book_name}を予約しました"
     end
   end
 
   def destroy
     favorite = current_user.favorites.find_by(book_id: params[:book_id]).destroy
-    redirect_to books_url, notice: "#ブログをお気に入り解除しました"
+    redirect_to books_url, notice: "#{favorite.book.book_name}の予約をキャンセルしました"
   end
 
 end
--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -2,6 +2,6 @@ class BookingMailer < ApplicationMailer
 def booking_mail(booking)
 
 
- mail to: "gooday46492000@yahoo.co.jp", subject: "お問い合わせの確認メール"
+ mail to: "gooday46492000@yahoo.co.jp", subject: "予約の確認メール"
 end
 end
