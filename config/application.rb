947b969de236a4d4da246701479d348768dd52b6
takuya <takuyamishima@gmail.com>
2018-05-28 23:32:16 +0000

    日本語化

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,13 @@ Bundler.require(*Rails.groups)
 module Library
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-config.i18n.default_locale = :ja
-config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    
+    
+    
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
+    config.i18n.default_locale = :ja
     # -- all .rb files in that directory are automatically loaded.
   end
 end
